405a9c3cf273e03dd6be91a5eb2df7f4cf52bd7f
Javid Ameri <javid.ameri@studio.unibo.it>
2023-11-20 23:26:06 +0100

    created the header-class Button.h and its implementation ButtonImpl.cpp

---
+++ Assignment 2/src/devices/Button.h
@@ -0,0 +1,21 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "Arduino.h"
+
+class Button {
+public:
+  Button(int pin);
+
+  void begin();
+  bool isPressed();
+  bool wasPressed();
+  bool wasReleased();
+
+private:
+  int pin;
+  bool currentState;
+  bool lastState;
+};
+
+#endif
