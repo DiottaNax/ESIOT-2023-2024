364c296e7b0f15ce2ff2f439fb07f547784191d4
Javid Ameri <javid.ameri@studio.unibo.it>
2023-11-20 23:25:42 +0100

    created the header-class Led.h and its implementation LedImpl.cpp

---
+++ Assignment 2/src/devices/Led.h
@@ -0,0 +1,19 @@
+#ifndef LED_H
+#define LED_H
+
+#include "Arduino.h"
+
+class Led {
+public:
+  Led(int pin);
+
+  void initialize();
+  void turnOn();
+  void turnOff();
+  void setIntensity(int intensity);
+
+private:
+  int pin;
+};
+
+#endif
---
+++ Assignment 2/src/devices/LedImpl.cpp
@@ -0,0 +1,22 @@
+#include "Led.h"
+
+Led::Led(int pin) {
+  this->pin = pin;
+}
+
+void Led::initialize() {
+  pinMode(pin, OUTPUT);
+}
+
+void Led::turnOn() {
+  digitalWrite(pin, HIGH);
+}
+
+void Led::turnOff() {
+  digitalWrite(pin, LOW);
+}
+
+void Led::setIntensity(int intensity) {
+  intensity = map(intensity, 0, 1023, 0, 255);
+  analogWrite(pin, intensity);
+}
