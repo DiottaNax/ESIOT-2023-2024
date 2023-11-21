092cf7cdfc6fd4f557ce075a00155a63d57e2ae1
matidan01 <matildedantino@gmail.com>
2023-11-21 17:14:38 +0100

    Added BlinkingTask, TaskWithState and ControllerTask

---
+++ Assignment 2/include/tasks/TaskWithState.h
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#ifndef __TASK_WITH_STATE__
+#define __TASK_WITH_STATE__
+
+#include "Task.h"
+
+class TaskWithState : Task {
+    
+public:
+  TaskWithState() : Task(){}
+
+  void setState(int state){
+      this->state = state;
+      stateTimestamp = millis();
+  }
+
+  int getState() {
+    return this->state;
+  }
+
+  long elapsedTimeInState(){
+      return millis() - stateTimestamp;
+  }
+  
+protected:
+  int state;
+  long stateTimestamp;
+};
+
+#endif
