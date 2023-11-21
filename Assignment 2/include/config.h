105bde9e2eb291f8236a44c0501adcc801b336dc
Nax <federico.diotallevi@studio.unibo.it>
2023-11-21 20:27:44 +0100

    added configuration file

---
+++ Assignment 2/include/config.h
@@ -0,0 +1,19 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define N2 2000
+#define N4 2000
+
+#define MINDIST 0.05
+#define MAXDIST 0.1
+
+enum CarWashingState {
+    CAR_WAITING,
+    WELCOME,
+    GATE_OPENING,
+    READY_TO_WASH,
+    CAR_WASHING,
+    WASHING_COMPLITED
+};
+
+#endif
