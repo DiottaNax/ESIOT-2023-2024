0772260d3fe77f85938b4fdeb5038c4bbd093f05
Javid Ameri <javid.ameri@studio.unibo.it>
2023-11-21 17:14:11 +0100

    did doc of Button.h , LCDDisplay.h , Led.h

--- Assignment 2/include/devices/Led.h
+++ Assignment 2/include/devices/Led.h
@@ -3,16 +3,35 @@
 
 #include <Arduino.h>
 
+/**
+ * @brief Represents a class for controlling an LED.
+ */
 class Led {
 public:
+  /**
+   * @brief Constructor for the Led class.
+   * @param pin The pin to which the LED is connected.
+   */
   Led(int pin);
 
+  /**
+   * @brief Turns on the LED.
+   */
   void turnOn();
+
+  /**
+   * @brief Turns off the LED.
+   */
   void turnOff();
+
+  /**
+   * @brief Sets the intensity of the LED.
+   * @param intensity The intensity value (0 to 255).
+   */
   void setIntensity(int intensity);
 
 private:
-  int pin;
+  int pin; /**< The pin to which the LED is connected. */
 };
 
 #endif
