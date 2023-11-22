259690b71204240856e1dbe2c2b6e1fe5c3aa807
Nax <federico.diotallevi@studio.unibo.it>
2023-11-22 23:00:29 +0100

    started implementation of switch cases

--- Assignment 2/include/tasks/UserConsoleTask.h
+++ Assignment 2/include/tasks/UserConsoleTask.h
@@ -1,20 +1,30 @@
 #ifndef __USER_CONSOLE_TASK__
 #define __USER_CONSOLE_TASK__
 
+#define RS_PIN 9
+#define ENABLE_PIN 8
+#define D4_PIN 4
+#define D5_PIN 5
+#define D6_PIN 6
+#define D7_PIN 7
+#define BUTTON_PIN 10
+
 #include "tasks/Task.h"
 #include "model/Bridge.h"
 #include "devices/Button.h"
 #include "devices/LCDDisplay.h"
+#include "devices/Button.h"
 #include "devices/Led.h"
 
 class UserConsoleTask : public Task {
 private:
     Bridge *_bridge;
-    LCDDisplay *_display;
-    Led *_leds[3];
+    LCDDisplay _display = LCDDisplay(RS_PIN, ENABLE_PIN, D4_PIN, D5_PIN, D6_PIN, D7_PIN);
+    Button _button = Button(BUTTON_PIN);
+    Led _leds[3] = {Led(PIN_LED1), Led(PIN_LED2), Led(PIN_LED3)};
 
 public:
-    UserConsoleTask(Bridge *bridge, LCDDisplay *display);
+    UserConsoleTask(Bridge *bridge);
     void init();
     void tick();
 };
--- Assignment 2/src/tasks/UserConsoleTask.cpp
+++ Assignment 2/src/tasks/UserConsoleTask.cpp
@@ -1,40 +1,43 @@
 #include <Arduino.h>
 #include "tasks/UserConsoleTask.h"
+#include "devices/LCDDisplay.h"
 #include "config.h"
 
-UserConsoleTask::UserConsoleTask(Bridge *bridge, LCDDisplay *display) {
+UserConsoleTask::UserConsoleTask(Bridge *bridge) {
     _bridge = bridge;
-    _display = display;
-    _leds[0] = new Led(PIN_LED1);
-    _leds[1] = new Led(PIN_LED1);
-    _leds[2] = new Led(PIN_LED1);
 }
 
 void UserConsoleTask::init(){
-    _display->initialize();
-    _display->clear();
+    _display.initialize();
+    _display.clear();
     for (int i = 0; i < 3; i++){
-        _leds[i]->turnOff();
+        _leds[i].turnOff();
     }
 }
 
 void UserConsoleTask::tick(){
     switch (_bridge->getState()) {
     case WELCOME:
-        _display->clear();
-        _display->print("Welcome", 0, 0);
+        _display.clear();
+        _display.print("Welcome", 0, 0);
         setActive(false);
         break;
     
     case GATE_OPENING:
-        _display->clear();
-        _display->print("Proceed to", 0, 0);
-        _display->print("the Washing Area", 0, 1);
+        _display.clear();
+        _display.print("Proceed to", 0, 0);
+        _display.print("the Washing Area", 0, 1);
         setActive(false);
         break;
-        
+
+    case READY_TO_WASH:
+        _display.clear();
+        _display.print("Ready to Wash", 0, 0);
+        setActive(false);
+        break;
+
     default:
-        _display->clear();
+        _display.clear();
         break;
     }
 }
