40b2f4b56653a705aaa759e4d363d11b545a8ac0
matidan01 <matildedantino@gmail.com>
2023-11-22 11:20:04 +0100

    Added Scheduler.cpp

--- Assignment 2/src/tasks/BlinkingTask.cpp
+++ Assignment 2/src/tasks/BlinkingTask.cpp
@@ -5,7 +5,6 @@ void BlinkingTask::init(int pin) {
     this->active = true;
     this->timeElapsed = 0;
     this->led = new Led(pin);
-    this->led->setIntensity(LOW);
     this->state = OFF;
 }
 
@@ -13,11 +12,11 @@ void BlinkingTask::tick() {
     switch(this->state) {
         case ON:
             this->led->turnOn();
-            state = ON; 
+            state = OFF; 
             break;
         case OFF:
             this->led->turnOff();
-            state = OFF;
+            state = ON;
             break;
     }
 }
