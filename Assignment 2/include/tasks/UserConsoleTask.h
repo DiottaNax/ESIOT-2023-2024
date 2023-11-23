05baeb1639ac85c6cb0cc9538f7c80619dbe2670
Nax <federico.diotallevi@studio.unibo.it>
2023-11-24 00:03:27 +0100

    implemented user console behaviour. Completed lcd behaviour and button behaviour. Need to finish the implementation of blinking task.

--- Assignment 2/include/tasks/UserConsoleTask.h
+++ Assignment 2/include/tasks/UserConsoleTask.h
@@ -1,8 +1,11 @@
 #ifndef __USER_CONSOLE_TASK__
 #define __USER_CONSOLE_TASK__
 
-#define RS_PIN 9
-#define ENABLE_PIN 8
+#define SLIDING_PERIOD 1000
+#define TICK_PERIOD 200
+
+#define RS_PIN 2
+#define ENABLE_PIN 3
 #define D4_PIN 4
 #define D5_PIN 5
 #define D6_PIN 6
@@ -10,6 +13,7 @@
 #define BUTTON_PIN 10
 
 #include "tasks/Task.h"
+#include "tasks/BlinkingTask.h"
 #include "model/Bridge.h"
 #include "devices/Button.h"
 #include "devices/LCDDisplay.h"
@@ -18,13 +22,22 @@
 
 class UserConsoleTask : public Task {
 private:
+    short _timeStamp;
+    bool _justChangedState;
+    String _tooLongString;
+
     Bridge *_bridge;
+    BlinkingTask *_tBlinking;
+
     LCDDisplay _display = LCDDisplay(RS_PIN, ENABLE_PIN, D4_PIN, D5_PIN, D6_PIN, D7_PIN);
     Button _button = Button(BUTTON_PIN);
     Led _leds[3] = {Led(PIN_LED1), Led(PIN_LED2), Led(PIN_LED3)};
 
+    int getWashingPercentage();
+    void makeTextScroll();
+
 public:
-    UserConsoleTask(Bridge *bridge);
+    UserConsoleTask(Bridge *bridge, BlinkingTask* tBlinking);
     void init();
     void tick();
 };
