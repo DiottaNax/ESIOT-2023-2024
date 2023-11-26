c55626eeb6143db57e47d83dc7b608514a0b6616
Nax <federico.diotallevi@studio.unibo.it>
2023-11-26 16:08:40 +0100

    added logging to errors and moved arduino rebooting routine inside

--- Assignment 2/java/src/smart/car/washing/serial/ChannelMonitor.java
+++ Assignment 2/java/src/smart/car/washing/serial/ChannelMonitor.java
@@ -1,39 +1,59 @@
 package smart.car.washing.serial;
 
-import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import smart.car.washing.controller.Controller;
+import smart.car.washing.gui.Dashboard;
+import smart.car.washing.gui.DashboardLogger;
 
 public class ChannelMonitor implements Runnable {
-    private Optional<SerialCommChannel> commChannel = Optional.empty();
-    private final Controller controller;
+    private final CommChannel commChannel;
+    private Controller controller;
 
-    public ChannelMonitor(final String port, final Controller controller) {
+    public ChannelMonitor(final CommChannel commChannel) {
+        this.commChannel = commChannel;
+    }
+
+    public void sendMessage(final String msg) {
+        commChannel.sendMsg(msg);
+    }
+
+    public void attachController(final Controller controller) {
         this.controller = controller;
-
-        try {
-            commChannel = Optional.of(new SerialCommChannel(port, 9600));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void run() {
-        if (commChannel.isPresent()) {
-            if (commChannel.get().isMsgAvailable()) {
-                try {
-                    this.controller.notifyEvent(commChannel.get().receiveMsg());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        try {
-            Thread.sleep(10);
+        System.out.println("Waiting Arduino for rebooting...");
+		try {
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+        System.out.println("Ready.");
+        
+        /**
+         * Listening for serial events
+         */
+        while (true) {
+            if (commChannel.isMsgAvailable()) {
+                try {
+                    String msg = commChannel.receiveMsg();
+                    System.out.println("\t" + msg);
+                    this.controller.notifyEvent(msg);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    DashboardLogger.showError(e, Level.SEVERE);
+                }
+            }
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                DashboardLogger.showError(e, Level.SEVERE);
+            }
         }
     }
 
