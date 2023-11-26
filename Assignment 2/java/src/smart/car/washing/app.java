2b83ea2a8ac64da4f6692b4205ac1eb7cd035229
Nax <federico.diotallevi@studio.unibo.it>
2023-11-26 16:09:26 +0100

    removed commented parts and

--- Assignment 2/java/src/smart/car/washing/app.java
+++ Assignment 2/java/src/smart/car/washing/app.java
@@ -1,45 +1,40 @@
 package smart.car.washing;
 
-import java.util.StringTokenizer;
+import java.util.logging.Level;
+
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 import smart.car.washing.controller.Controller;
 import smart.car.washing.controller.ControllerImpl;
-import smart.car.washing.gui.ConsoleGui;
+import smart.car.washing.gui.Dashboard;
+import smart.car.washing.gui.DashboardLogger;
+import smart.car.washing.gui.SmartCarWashingDashboard;
 import smart.car.washing.serial.ChannelMonitor;
+import smart.car.washing.serial.SerialCommChannel;
 
 public class app {
+
+    private static ChannelMonitor channelMonitor;
+    private static Thread channelMonitorThread;
+    private static Controller controller;
+    private static Dashboard dashboard; 
     
-    public static void main(String[] args) throws Exception {
-        final ConsoleGui gui = new ConsoleGui();
-        final Controller controller = new ControllerImpl(gui);
-        gui.attachController(controller);
-        final Thread channelMonitorThread = new Thread(new ChannelMonitor("COM9", controller));
-        channelMonitorThread.start();
+    public static void main(String[] args) {
+        dashboard = new SmartCarWashingDashboard();
         
-		System.out.println("Waiting Arduino for rebooting...");
-		Thread.sleep(3000);
-        System.out.println("Ready.");
-        while (true) {
-            /*try{
-                String msg = channel.receiveMsg();
-                System.err.println("Il messaggio ricevuto è"+ msg);
-                StringTokenizer tokenizer = new StringTokenizer(msg,":");
-                switch(tokenizer.nextToken()){
-                    case "TEMP": 
-                        gui.temp.setText("Current Temperature: "+Integer.parseInt(tokenizer.nextToken()));
-                        break;
-                    case "STATE":
-                        gui.state.setText("Current State: "+tokenizer.nextToken());
-                        break;
-                    case "NUMBER":
-                        gui.state.setText("Current Number: "+Integer.parseInt(tokenizer.nextToken()));
-                        break;
-                }
-                Thread.sleep(5);
-            } catch(NumberFormatException e){
-                System.out.println("Non è stato possibile convertire il numero: ");
-                e.printStackTrace();
-            }*/
+        try {
+            channelMonitor = new ChannelMonitor(new SerialCommChannel("COM9", 9600));
+            channelMonitor.attachController(controller);
+        } catch (Exception e) {
+            e.printStackTrace();
+            DashboardLogger.showError(e, Level.SEVERE);
         }
+
+        channelMonitorThread = new Thread(channelMonitor);
+        controller = new ControllerImpl(dashboard, channelMonitor);
+        channelMonitor.attachController(controller);
+        dashboard.attachController(controller);
+        channelMonitorThread.start();
     }
 }
