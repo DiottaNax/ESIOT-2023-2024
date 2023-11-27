393c035873180952a28bdda37e0316f7c8e86b49
matidan01 <matildedantino@gmail.com>
2023-11-27 02:00:16 +0100

    Main arrangement

--- Assignment 2/src/main.cpp
+++ Assignment 2/src/main.cpp
@@ -6,17 +6,21 @@
 #include "tasks/DistanceControlTask.h"
 #include "tasks/TemperatureTask.h"
 #include "devices/PresenceDetectorImpl.h"
+#include "devices/Gate.h"
+#include "devices/UltrasonicSensor.h"
+#include "tasks/UserConsoleTask.h"
 #include "config.h"
 
-//#include <EnableInterrupt.h>
 #include <avr/sleep.h>
 
 Bridge *bridge;
 Scheduler *scheduler;
+
 ControllerTask *controller;
 BlinkingTask *blink;
 TemperatureTask *tempController;
-PresenceDetectorImpl *presenceSensor;
+DistanceControlTask *distanceController;
+UserConsoleTask *userConsole;
 
 void changeState() {
   Serial.println("Movement detected");
@@ -24,17 +28,32 @@ void changeState() {
 
 void setup() {
   Serial.begin(9600);
+
+  Gate *gate = new Gate(GATE_PIN);
+  UltrasonicSensor *distanceSensor = new UltrasonicSensor(DISTANCE_ECHO_PIN, DISTANCE_TRIGGERED_PIN);
+
   bridge = new Bridge();
-  scheduler = new Scheduler();
+  
   blink = new BlinkingTask();
   tempController = new TemperatureTask(50, bridge);
-  presenceSensor = new PresenceDetectorImpl(PIR_PIN);
+  tempController->init(LM35_PIN);
+  distanceController = new DistanceControlTask(distanceSensor, gate, bridge);
+  distanceController->init();
+  userConsole = new UserConsoleTask(bridge, blink);
+  userConsole->init();
 
-  controller = new ControllerTask(bridge, blink, tempController, presenceSensor);
+  controller = new ControllerTask(bridge, userConsole, tempController, distanceController);
   controller->init();
   controller->setActive(true);
+
+  scheduler = new Scheduler();
   scheduler->init(10);
   scheduler->addTask(controller);
+  scheduler->addTask(blink);
+  scheduler->addTask(tempController);
+  scheduler->addTask(distanceController);
+  scheduler->addTask(userConsole);
+
   attachInterrupt(digitalPinToInterrupt(PIR_PIN), changeState, CHANGE);
 }
 
