e6e72551bb0149bca432f662ee83ad18b37f68ef
matidan01 <matildedantino@gmail.com>
2023-11-27 15:21:06 +0100

    Added lastStateTimeStamp

--- Assignment 2/include/model/Bridge.h
+++ Assignment 2/include/model/Bridge.h
@@ -2,17 +2,56 @@
 #define __BRIDGE__
 #include "config.h"
 
+/**
+ * @brief Represents a bridge that tracks the state and elapsed time in the car washing process.
+ *
+ * The Bridge class is designed to manage the state of a car washing process and track the elapsed
+ * time spent in each state. 
+ */
 class Bridge {
 
 private:
-    CarWashingState currentState;
-    long stateTimestamp;
+    CarWashingState currentState; //The current state of the car washing process.
+    long stateTimestamp; //The timestamp of the last state transition.
+    long lastStateTimeStamp;
 
 public:
+
+    /**
+     * @brief Default constructor for the Bridge class.
+     *
+     * Initializes the Bridge with an initial state.
+     */
     Bridge();
+
+    /**
+     * @brief Get the current state of the car washing process.
+     *
+     * @return The current state represented by the CarWashingState enum.
+     */
     CarWashingState getState();
+
+    /**
+     * @brief Set the current state of the car washing process.
+     *
+     * @param state The new state to set.
+     */
     void setState(CarWashingState);
+
+    /**
+     * @brief Get the elapsed time in the current state.
+     *
+     * This function calculates and returns the time elapsed (in milliseconds)
+     * since the last state transition.
+     *
+     * @return The elapsed time in the current state.
+     */
     long elapsedTimeInState();
+
+    long elapsedTimeInLastState();
+
+    void changeElapsedTimeInState(long time);
+
 };
 
 #endif
--- Assignment 2/src/model/Bridge.cpp
+++ Assignment 2/src/model/Bridge.cpp
@@ -4,11 +4,16 @@
 
 Bridge::Bridge() {
     this->currentState = CAR_WAITING;
+    this->elapsedTimeInLastState = 0;
 }
 
 void Bridge::setState(CarWashingState newState) {
     this->currentState = newState;
+    if (newState != MAINTENANCE) {
+        this->elapsedTimeInLastState = this->elapsedTimeInState;
+    }
     stateTimestamp = millis();
+    MsgService.sendMsg("STATE:"+this->currentState);
 }
 
 CarWashingState Bridge::getState() {
@@ -17,4 +22,8 @@ CarWashingState Bridge::getState() {
 
 long Bridge::elapsedTimeInState() {
     return (millis() - this->stateTimestamp);
+}
+
+void Bridge::changeElapsedTimeInState(long time) {
+    this->stateTimestamp=time;
 }
