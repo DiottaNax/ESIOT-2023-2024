f9b9d20f44a2e4efb5271162ae565fc91d7815cc
matidan01 <matildedantino@gmail.com>
2023-11-27 15:22:00 +0100

    Added documentation

--- Assignment 2/include/tasks/ControllerTask.h
+++ Assignment 2/include/tasks/ControllerTask.h
@@ -11,17 +11,54 @@
 
 #define PERIOD 10
 
-class ControllerTask : public TaskWithState {
+/**
+ * @brief Represents a controller task coordinating temperature, user console, and distance control tasks.
+ *
+ * The ControllerTask class is a specific implementation of the Task interface
+ * responsible for coordinating the activities of temperature control, user console interaction,
+ * and distance control. It acts as the central controller for the overall system.
+ */
+class ControllerTask : public Task {
+
 private:
-    TemperatureTask *tempController;
-    UserConsoleTask *userConsole;
-    DistanceControlTask *distanceController;
-    Bridge *bridge;
+
+    TemperatureTask *tempController; //Pointer to the temperature control task.
+    UserConsoleTask *userConsole; //Pointer to the user console interaction task.
+    DistanceControlTask *distanceController; //Pointer to the distance control task.
+    Bridge *bridge; //Pointer to the bridge managing state and time in the car washing process.
+
+    int washingNumber = 0; //The current washing number.
 
 public:
+
+    /**
+     * @brief Constructor for ControllerTask.
+     *
+     * Initializes the ControllerTask with the necessary tasks and bridge.
+     *
+     * @param bridge Pointer to the Bridge managing state and time.
+     * @param userConsole Pointer to the UserConsoleTask for user interaction.
+     * @param tempController Pointer to the TemperatureTask for temperature control.
+     * @param distanceController Pointer to the DistanceControlTask for distance control.
+     */
     ControllerTask(Bridge *bridge, UserConsoleTask *userConsole, TemperatureTask *tempController, DistanceControlTask *distanceController);
+
+    /**
+     * @brief Initialize the ControllerTask.
+     *
+     * This function initializes the ControllerTask and its associated tasks.
+     */
     void init();
+
+    /**
+     * @brief Perform a single iteration of the controller task.
+     *
+     * This function is called on each iteration of the task and coordinates
+     * the activities of temperature control, user console interaction, and distance control.
+     * It activates or deactivates tasks based on the state of the bridge.
+     */
     void tick();
+
 };
 
 #endif
