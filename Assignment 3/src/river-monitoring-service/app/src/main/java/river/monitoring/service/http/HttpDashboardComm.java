5ef8d566138825f1e88ebbe6fd0939969a4b5217
Nax <federico.diotallevi@studio.unibo.it>
2024-04-16 10:10:35 +0200

    [FIX] fixed paths in handlers that caused the thread death, fixed remote control request that made not possible for the dasboard to set auto control

--- Assignment 3/src/river-monitoring-service/app/src/main/java/river/monitoring/service/http/HttpDashboardComm.java
+++ Assignment 3/src/river-monitoring-service/app/src/main/java/river/monitoring/service/http/HttpDashboardComm.java
@@ -1,6 +1,7 @@
 package river.monitoring.service.http;
 
 import java.util.Optional;
+//import java.util.Random; // FOR TESTING
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
@@ -11,6 +12,7 @@ import river.monitoring.service.api.DashboardCommunicator;
 import river.monitoring.service.api.SystemModeHandler;
 import river.monitoring.service.common.DataPoint;
 import river.monitoring.service.common.SystemMode;
+//import river.monitoring.service.common.SystemState; //FOR TESTING
 
 public class HttpDashboardComm extends AbstractVerticle implements DashboardCommunicator {
     private final static int STATUS_OK = 200;
@@ -37,19 +39,15 @@ public class HttpDashboardComm extends AbstractVerticle implements DashboardComm
         // Create a Router
         Router router = Router.router(vertx);
 
-        // CORS handler
-        CorsHandler corsHandler = CorsHandler.create("*"); // Allow requests from any origin
-
         // Mount the CORS handler
-        router.route().handler(corsHandler);
+        //router.route().handler(BodyHandler.create());
+        router.route().handler(CorsHandler.create("*")); // Allow requests from any origin
+        // Mount the handler for incoming http request by the dashboard
+        router.route("/api/data").handler(this::handleGetData);
+        router.route("/api/mode").handler(this::handleRemoteControlRequest);
+        router.route("/api/valve").handler(this::setValveOpening);
 
-        // Mount the handler for all incoming requests at application/data for every HTTP request
-        router.route("api/data").handler(this::handleGetData);
-
-        router.route("api/mode").handler(this::handleRemoteControlRequest);
-
-        router.route("api/valve").handler(this::setValveOpening);
-
+        log("creating http server");
         // Create the HTTP server
         vertx.createHttpServer()
                 // Handle every request using the router
@@ -57,60 +55,52 @@ public class HttpDashboardComm extends AbstractVerticle implements DashboardComm
                 // Start listening
                 .listen(PORT)
                 // Print the port
-                .onSuccess(server -> System.out.println("HTTP server started on port " + server.actualPort()));
-    }
-
-    public void addNewData(final DataPoint data) {
-        this.data = Optional.of(data);
+                .onSuccess(server -> log("HTTP server started on port " + server.actualPort()));
     }
 
     private void handleGetData(final RoutingContext context) {
-        String address = context.request().connection().remoteAddress().toString();
-        // Get the query parameter "name"
-        log("REQUEST FROM: " + address);
-        log("DATA: " + context.request().params());
+        // FOR TESTING WITHOUT ARDUINO OR ESP DECOMMENT BELOW
+        /*double wLevel = new Random().nextDouble(1.7, 30.0);
+        var testData = new DataPoint(this.modeHandler.getCurrentMode(), wLevel,
+                SystemState.NORMAL.getValveOpening(), SystemState.NORMAL);
+        var data = Optional.of(testData);*/
 
         // Check if data is present
         if (data.isPresent()) {
             // Process data if present
             context.response()
                     .putHeader("content-type", "api/json")
-                    .end(data.get().getAsJson().encodePrettily());
+                    .end(data.get().asJson().encodePrettily());
         } else {
             // Return error if data is not present
-            context.fail(STATUS_RESOURCE_NOT_FOUND); // Set error code 404, data not found
+            context.response().setStatusCode(STATUS_RESOURCE_NOT_FOUND).end(); // Set error code 404, data not found
         }
     }
     
     private void handleRemoteControlRequest(final RoutingContext context) {
-        // Get the address of the request
-        String address = context.request().connection().remoteAddress().toString();
-        // Get the query parameter "name"
-        log("REQUEST FROM: " + address);
-        log("DATA: " + context.request().params());
-
-        final boolean success = modeHandler.changeMode(SystemMode.REMOTE);
-        final int statusCode = success ? STATUS_OK : STATUS_FORBIDDEN; // If mode is changed successfully then 200(ok) is setted as status code, otherwise 403(forbidden) is setted
-        context.response().setStatusCode(statusCode).end();
+        // Get the next mode to set, if the current is AUTO the next is REMOTE and vice-versa 
+        final SystemMode nextMode = modeHandler.getCurrentMode().equals(SystemMode.REMOTE)
+                ? SystemMode.AUTO
+                : SystemMode.REMOTE;
+ 
+        final boolean success = modeHandler.changeMode(nextMode);
+        // If mode is changed successfully then 200(ok) is setted as status code, otherwise 403(forbidden) is setted
+        final int statusCode = success ? STATUS_OK : STATUS_FORBIDDEN;
+        log(this.modeHandler.getCurrentMode().asJson().encodePrettily());
+        // End the request with the correct status code and the current system mode wrapped into a json
+        context.response()
+                .setStatusCode(statusCode)
+                .end(this.modeHandler.getCurrentMode().asJson().encodePrettily());
     }
     
     private void setValveOpening(final RoutingContext context) {
-        // Get the address of the request
-        String address = context.request().connection().remoteAddress().toString();
-        // Get the query parameter "name"
-        log("REQUEST FROM: " + address);
-        log("DATA: " + context.request().params());
-
+        log("trying to set valve, message content: " + context.request().body());
         var body = context.body().asJsonObject();
         if (body != null && body.containsKey("VALVE_OPENING")) {
             this.valveOpening = Optional.of(body.getInteger("VALVE_OPENING"));
         } else {
             log("Errore: Parametro VALVE_OPENING mancante nella richiesta.");
         }
-    }
-
-    private void log(final String message) {
-        System.out.println("[HTTP_COMMUNICATOR]: " + message);
     }
 
     @Override
@@ -121,5 +111,9 @@ public class HttpDashboardComm extends AbstractVerticle implements DashboardComm
     @Override
     public void updateData(Optional<DataPoint> data) {
         this.data = data;
+    }
+
+    private void log(final String message) {
+        System.out.println("[HTTP_COMMUNICATOR]: " + message);
     }
 }
