07e9bbc5bdbf95a6886d715df18207384f1af53b
Nax <federico.diotallevi@studio.unibo.it>
2024-05-15 18:51:53 +0200

    [DOC] dashboard code revision

--- Assignment 3/src/river-monitoring-dashboard/dashboard.js
+++ Assignment 3/src/river-monitoring-dashboard/dashboard.js
@@ -1,73 +1,85 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    let oldData = new Array();
+    let oldData = new Array(); // Saves water levels
     let labels = new Array();
-    let graph;
+    let graph; // Graph for water levels
 
-    const length = 10;
+    const length = 10; // lenght of the graph (and old data)
 
-    let bar = document.getElementById('valveOpening');
-    bar.disabled = true;
+    let bar = document.getElementById('valveOpening'); // slider for remote control
+    bar.disabled = true; // the slider starts disabled until some data are collected
     let barValue = document.getElementById('valveValue');
     let remoteButton = document.getElementById('remoteButton');
     const canvas = document.getElementById('graph');
     let valveOpeningText = document.getElementById('valveOpeningText');
+    let valveOpeningLevel = 0;
 
-    const int = 5000; 
-    const syncRequest = setInterval(getData, int);
+    const frequency = 5000; // Interval to get data in millis
+    const syncRequest = setInterval(getData, frequency);
 
+    // Convert the angle got from service into percentage
+    function toPerc(angle) {
+        const toPerc = 1.8;
+        return Math.round(angle / toPerc);
+    }
+
+    // if a click is detected send a request to service to get remote control
     remoteButton.addEventListener('click', function() {
         let data = 'MODE_REMOTE';
-        
+
         axios.post('http://localhost:8080/api/mode', data)
-            .then(response => {
-                changeMode(response.data[0].MODE);
+            .then(response => { // If remote control is set
+                bar.value = valveOpeningLevel; // set bar value as the last valve opening level got from the service
+                barValue.textContent = toPerc(valveOpeningLevel) + "%";
+                changeMode(response.data[0].MODE); // Set new mode
             })
             .catch(error => {
             });
     });
     
-    bar.addEventListener('change', function() {
-        let value = bar.value;
+    bar.addEventListener('mouseup', function() { // On bar released
+        let value = bar.value; // Get setted valve opening
  
-        barValue.textContent = value + '°';
+        barValue.textContent = value + '%'; // set slider label
 
-        changeValveOpening(value);
+        changeValveOpening(value); // set valve info label
 
         let data = {
             "VALVE_OPENING": value
         };
 
-        console.log(data);
+        //console.log(data); // DEBUG
 
-        axios.post('http://localhost:8080/api/valve', data)
+        axios.post('http://localhost:8080/api/valve', data) // Send valve opening to service
             .then(response => {
-                console.log(response.data);
+                //console.log(response.data); // DEBUG
             })
             .catch(error => {
             });
     });
-    
+
+    // request data from the service
     function getData() {
         let data = {};
 
         axios
           .post("http://localhost:8080/api/data", data)
           .then((response) => {
-            const responseData = response.data[0];
+            const responseData = response.data[0]; // Get json response from the service
             const timestamp = new Date().toLocaleTimeString("en-US", {
-              hour12: false,
-            }); // Use 24-hour format for clarity
-
+              hour12: false, // 24-hours format
+            }); // Set date to show in the graph
             changeMode(responseData.MODE);
             changeSystemState(responseData.SYSTEM_STATE);
             changeWaterLevel(responseData.WATER_LEVEL, timestamp);
             changeValveOpening(responseData.VALVE_OPENING);
+            valveOpeningLevel = responseData.VALVE_OPENING;
           })
           .catch((error) => {
             console.error("Error fetching data:", error);
           });
     }
 
+    // Set component enabled/disabled, visible/unvisible and text based on the new mode set
     function changeMode(newMode) {
         if (newMode == 'REMOTE') {
             bar.disabled = false;
@@ -92,14 +104,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
+    // Changes system state label (i.e. alarm level)
     function changeSystemState(newState) {
         document.getElementById('system_state').textContent = "STATE: " + newState;
     }
 
+    // Updates graph
     function changeWaterLevel(newLevel, timestamp) {
         updateData(oldData, newLevel);
         updateData(labels, timestamp);
 
+        // Create graph if it's null
         if (!graph) {
             const ctx = canvas.getContext("2d");
 
@@ -135,21 +150,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 },
             },
             });
-        } else {
+        } else { // update graph's dataset if the graph is not null
             graph.data.datasets[0].data = oldData;
             graph.data.labels = labels;
             graph.update();
         }
     }
 
+    // Update dataset adding the new water level to the array
     function updateData(data, newData) {
       data.push(newData);
-      if (data.length > length) {
-        data.shift();
+      if (data.length > length) { // if array lenght is greater from the chosen graph size
+        data.shift(); // drop the first item (the oldest)
       }
     }
 
     function changeValveOpening(newValue) {
-        document.getElementById('valve_opening_value').textContent = "VALVE OPENING: " + newValue + "%";
+        document.getElementById('valve_opening_value').textContent = "VALVE OPENING: " + toPerc(newValue) + "%";
     }
 });
